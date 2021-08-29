1dca58f516e10e1c4a58ddd45c228527a6770a9d
hilalsidhic <hilalsidhic21@gmail.com>
2021-08-29 19:34:21 +0530

    file basics and pip command

---
+++ file_sample.py
@@ -0,0 +1,7 @@
+# f=open("hilal.py","w")           #write
+# f.write("print('hilal')")
+# f.close()
+
+with open("hello.py","r") as f:     #read
+    x=f.read()
+print(x)
---
+++ html2pdf.py
@@ -0,0 +1,4 @@
+import pdfkit
+pdfkit.from_url('https://www.geeksforgeeks.org','htmltopdfguide.pdf')
+
+#https://en.terminalroot.com.br/the-30-best-python-libraries-and-packages-for-beginners/
